302e7d1be5f4e0db295820fc1eeefd68af40c600
Fred Gleason <fredg@paraelsystems.com>
2016-11-16 08:57:44 -0500

    2016-11-16 Fred Gleason <fredg@paravelsystems.com> 	* Moved dialog elementsfrom 'src/glassgui/' into 'src/common/'.

--- link_common.sh
+++ link_common.sh
@@ -42,25 +42,95 @@ ln -s ../../src/common/codec.cpp src/$DESTDIR/codec.cpp
 rm -f src/$DESTDIR/codec.h
 ln -s ../../src/common/codec.h src/$DESTDIR/codec.h
 
+rm -f src/$DESTDIR/codecdialog.cpp
+ln -s ../../src/common/codecdialog.cpp src/$DESTDIR/codecdialog.cpp
+rm -f src/$DESTDIR/codecdialog.h
+ln -s ../../src/common/codecdialog.h src/$DESTDIR/codecdialog.h
+
+rm -f src/$DESTDIR/codeviewer.cpp
+ln -s ../../src/common/codeviewer.cpp src/$DESTDIR/codeviewer.cpp
+rm -f src/$DESTDIR/codeviewer.h
+ln -s ../../src/common/codeviewer.h src/$DESTDIR/codeviewer.h
+
+rm -f src/$DESTDIR/combobox.cpp
+ln -s ../../src/common/combobox.cpp src/$DESTDIR/combobox.cpp
+rm -f src/$DESTDIR/combobox.h
+ln -s ../../src/common/combobox.h src/$DESTDIR/combobox.h
+
 rm -f src/$DESTDIR/connector.cpp
 ln -s ../../src/common/connector.cpp src/$DESTDIR/connector.cpp
 rm -f src/$DESTDIR/connector.h
 ln -s ../../src/common/connector.h src/$DESTDIR/connector.h
+
+rm -f src/$DESTDIR/hpiinputlistview.cpp
+ln -s ../../src/common/hpiinputlistview.cpp src/$DESTDIR/hpiinputlistview.cpp
+rm -f src/$DESTDIR/hpiinputlistview.h
+ln -s ../../src/common/hpiinputlistview.h src/$DESTDIR/hpiinputlistview.h
+
+rm -f src/$DESTDIR/hpiwidget.cpp
+ln -s ../../src/common/hpiwidget.cpp src/$DESTDIR/hpiwidget.cpp
+rm -f src/$DESTDIR/hpiwidget.h
+ln -s ../../src/common/hpiwidget.h src/$DESTDIR/hpiwidget.h
 
 rm -f src/$DESTDIR/logging.cpp
 ln -s ../../src/common/logging.cpp src/$DESTDIR/logging.cpp
 rm -f src/$DESTDIR/logging.h
 ln -s ../../src/common/logging.h src/$DESTDIR/logging.h
 
+rm -f src/$DESTDIR/messagewidget.cpp
+ln -s ../../src/common/messagewidget.cpp src/$DESTDIR/messagewidget.cpp
+rm -f src/$DESTDIR/messagewidget.h
+ln -s ../../src/common/messagewidget.h src/$DESTDIR/messagewidget.h
+
 rm -f src/$DESTDIR/metaevent.cpp
 ln -s ../../src/common/metaevent.cpp src/$DESTDIR/metaevent.cpp
 rm -f src/$DESTDIR/metaevent.h
 ln -s ../../src/common/metaevent.h src/$DESTDIR/metaevent.h
 
+rm -f src/$DESTDIR/profile.cpp
+ln -s ../../src/common/profile.cpp src/$DESTDIR/profile.cpp
+rm -f src/$DESTDIR/profile.h
+ln -s ../../src/common/profile.h src/$DESTDIR/profile.h
+
 rm -f src/$DESTDIR/ringbuffer.cpp
 ln -s ../../src/common/ringbuffer.cpp src/$DESTDIR/ringbuffer.cpp
 rm -f src/$DESTDIR/ringbuffer.h
 ln -s ../../src/common/ringbuffer.h src/$DESTDIR/ringbuffer.h
+
+rm -f src/$DESTDIR/segmeter.cpp
+ln -s ../../src/common/segmeter.cpp src/$DESTDIR/segmeter.cpp
+rm -f src/$DESTDIR/segmeter.h
+ln -s ../../src/common/segmeter.h src/$DESTDIR/segmeter.h
+
+rm -f src/$DESTDIR/serverdialog.cpp
+ln -s ../../src/common/serverdialog.cpp src/$DESTDIR/serverdialog.cpp
+rm -f src/$DESTDIR/serverdialog.h
+ln -s ../../src/common/serverdialog.h src/$DESTDIR/serverdialog.h
+
+rm -f src/$DESTDIR/sourcedialog.cpp
+ln -s ../../src/common/sourcedialog.cpp src/$DESTDIR/sourcedialog.cpp
+rm -f src/$DESTDIR/sourcedialog.h
+ln -s ../../src/common/sourcedialog.h src/$DESTDIR/sourcedialog.h
+
+rm -f src/$DESTDIR/spinbox.cpp
+ln -s ../../src/common/spinbox.cpp src/$DESTDIR/spinbox.cpp
+rm -f src/$DESTDIR/spinbox.h
+ln -s ../../src/common/spinbox.h src/$DESTDIR/spinbox.h
+
+rm -f src/$DESTDIR/statuswidget.cpp
+ln -s ../../src/common/statuswidget.cpp src/$DESTDIR/statuswidget.cpp
+rm -f src/$DESTDIR/statuswidget.h
+ln -s ../../src/common/statuswidget.h src/$DESTDIR/statuswidget.h
+
+rm -f src/$DESTDIR/stereometer.cpp
+ln -s ../../src/common/stereometer.cpp src/$DESTDIR/stereometer.cpp
+rm -f src/$DESTDIR/stereometer.h
+ln -s ../../src/common/stereometer.h src/$DESTDIR/stereometer.h
+
+rm -f src/$DESTDIR/streamdialog.cpp
+ln -s ../../src/common/streamdialog.cpp src/$DESTDIR/streamdialog.cpp
+rm -f src/$DESTDIR/streamdialog.h
+ln -s ../../src/common/streamdialog.h src/$DESTDIR/streamdialog.h
 
 rm -f src/$DESTDIR/glasslimits.h
 ln -s ../../src/common/glasslimits.h src/$DESTDIR/glasslimits.h
