1d95d2b39144357635b479c0cf57ceaece3a7526
Fred Gleason <fredg@paraelsystems.com>
2016-11-30 14:19:51 -0500

    2016-11-30 Fred Gleason <fredg@paravelsystems.com> 	* Added a codec for OggOpus in 'src/glasscoder/opuscodec.cpp' and 	'src/glasscoder/opuscodec.h'.

--- src/glasscoder/opuscodec.cpp
+++ src/glasscoder/opuscodec.cpp
@@ -26,6 +26,8 @@
 OpusCodec::OpusCodec(Ringbuffer *ring,QObject *parent)
   : Codec(Codec::TypeAac,ring,parent)
 {
+  opus_packet_number=0;
+  opus_packet_granulepos=0;
 }
 
 
@@ -47,7 +49,7 @@ QString OpusCodec::contentType() const
 
 unsigned OpusCodec::pcmFrames() const
 {
-  return 960;
+  return 2880;
 }
 
 
@@ -69,7 +71,7 @@ bool OpusCodec::startCodec()
   int err;
 
   //
-  // Load Library
+  // Load Libraries
   //
   opus_handle=dlopen("libopus.so",RTLD_LAZY);
 
@@ -84,6 +86,93 @@ bool OpusCodec::startCodec()
   *(void **)(&opus_encoder_destroy)=dlsym(opus_handle,"opus_encoder_destroy");
   *(void **)(&opus_encoder_ctl)=dlsym(opus_handle,"opus_encoder_ctl");
   *(void **)(&opus_strerror)=dlsym(opus_handle,"opus_strerror");
+
+  opus_ogg_handle=dlopen("libogg.so",RTLD_LAZY);
+  if(opus_ogg_handle==NULL) {
+    Log(LOG_ERR,"unsupported audio format (library not found)");
+    return false;
+  }
+  *(void **)(&oggpack_writeinit)=dlsym(opus_ogg_handle,"oggpack_writeint");
+  *(void **)(&oggpack_writecheck)=dlsym(opus_ogg_handle,"oggpack_writecheck");
+  *(void **)(&oggpack_writetrunc)=dlsym(opus_ogg_handle,"oggpack_writetrunc");
+  *(void **)(&oggpack_writealign)=dlsym(opus_ogg_handle,"oggpack_writealign");
+  *(void **)(&oggpack_writecopy)=dlsym(opus_ogg_handle,"oggpack_writecopy");
+  *(void **)(&oggpack_reset)=dlsym(opus_ogg_handle,"oggpack_reset");
+  *(void **)(&oggpack_writeclear)=dlsym(opus_ogg_handle,"oggpack_writeclear");
+  *(void **)(&oggpack_readinit)=dlsym(opus_ogg_handle,"oggpack_readinit");
+  *(void **)(&oggpack_write)=dlsym(opus_ogg_handle,"oggpack_write");
+  *(void **)(&oggpack_look)=dlsym(opus_ogg_handle,"oggpack_look");
+  *(void **)(&oggpack_look1)=dlsym(opus_ogg_handle,"oggpack_look1");
+  *(void **)(&oggpack_adv)=dlsym(opus_ogg_handle,"oggpack_adv");
+  *(void **)(&oggpack_adv1)=dlsym(opus_ogg_handle,"oggpack_adv1");
+  *(void **)(&oggpack_read)=dlsym(opus_ogg_handle,"oggpack_read");
+  *(void **)(&oggpack_read1)=dlsym(opus_ogg_handle,"oggpack_read1");
+  *(void **)(&oggpack_bytes)=dlsym(opus_ogg_handle,"oggpack_bytes");
+  *(void **)(&oggpack_bits)=dlsym(opus_ogg_handle,"oggpack_bits");
+    *(void **)(&oggpack_get_buffer)=
+      dlsym(opus_ogg_handle,"oggpack_get_buffer");
+  *(void **)(&oggpackB_writeinit)=dlsym(opus_ogg_handle,"oggpackB_writeinit");
+  *(void **)(&oggpackB_writecheck)=
+    dlsym(opus_ogg_handle,"oggpackB_writecheck");
+  *(void **)(&oggpackB_writetrunc)=
+    dlsym(opus_ogg_handle,"oggpackB_writetrunc");
+  *(void **)(&oggpackB_writealign)=
+    dlsym(opus_ogg_handle,"oggpackB_writealign");
+  *(void **)(&oggpackB_writecopy)=dlsym(opus_ogg_handle,"oggpackB_writecopy");
+  *(void **)(&oggpackB_reset)=dlsym(opus_ogg_handle,"oggpackB_reset");
+  *(void **)(&oggpackB_writeclear)=
+    dlsym(opus_ogg_handle,"oggpackB_writeclear");
+  *(void **)(&oggpackB_readinit)=dlsym(opus_ogg_handle,"oggpackB_readini");
+  *(void **)(&oggpackB_write)=dlsym(opus_ogg_handle,"oggpackB_write");
+  *(void **)(&oggpackB_look)=dlsym(opus_ogg_handle,"oggpackB_look");
+  *(void **)(&oggpackB_look1)=dlsym(opus_ogg_handle,"oggpackB_look1");
+  *(void **)(&oggpackB_adv)=dlsym(opus_ogg_handle,"oggpackB_adv");
+  *(void **)(&oggpackB_adv1)=dlsym(opus_ogg_handle,"oggpackB_adv1");
+  *(void **)(&oggpackB_read)=dlsym(opus_ogg_handle,"oggpackB_read");
+  *(void **)(&oggpackB_read1)=dlsym(opus_ogg_handle,"oggpackB_read1");
+  *(void **)(&oggpackB_bytes)=dlsym(opus_ogg_handle,"oggpackB_bytes");
+  *(void **)(&oggpackB_bits)=dlsym(opus_ogg_handle,"oggpackB_bits");
+  *(void **)(&oggpackB_get_buffer)=
+    dlsym(opus_ogg_handle,"oggpackB_get_buffer");
+  *(void **)(&ogg_stream_packetin)=
+    dlsym(opus_ogg_handle,"ogg_stream_packetin");
+  *(void **)(&ogg_stream_iovecin)=dlsym(opus_ogg_handle,"ogg_stream_iovecin");
+  *(void **)(&ogg_stream_pageout)=dlsym(opus_ogg_handle,"ogg_stream_pageout");
+  *(void **)(&ogg_stream_flush)=dlsym(opus_ogg_handle,"ogg_stream_flush");
+  *(void **)(&ogg_sync_init)=dlsym(opus_ogg_handle,"ogg_sync_init");
+  *(void **)(&ogg_sync_clear)=dlsym(opus_ogg_handle,"ogg_sync_clear");
+  *(void **)(&ogg_sync_reset)=dlsym(opus_ogg_handle,"ogg_sync_reset");
+  *(void **)(&ogg_sync_destroy)=dlsym(opus_ogg_handle,"ogg_sync_destroy");
+  *(void **)(&ogg_sync_check)=dlsym(opus_ogg_handle,"ogg_sync_check");
+  *(void **)(&ogg_sync_buffer)=dlsym(opus_ogg_handle,"ogg_sync_buffer");
+  *(void **)(&ogg_sync_wrote)=dlsym(opus_ogg_handle,"ogg_sync_wrote");
+  *(void **)(&ogg_sync_pageseek)=dlsym(opus_ogg_handle,"ogg_sync_pageseek");
+  *(void **)(&ogg_sync_pageout)=dlsym(opus_ogg_handle,"ogg_sync_pageout");
+  *(void **)(&ogg_stream_pagein)=dlsym(opus_ogg_handle,"ogg_stream_pagein");
+  *(void **)(&ogg_stream_packetout)=
+    dlsym(opus_ogg_handle,"ogg_stream_packetout");
+  *(void **)(&ogg_stream_packetpeek)=
+    dlsym(opus_ogg_handle,"ogg_stream_packetpeek");
+  *(void **)(&ogg_stream_init)=dlsym(opus_ogg_handle,"ogg_stream_init");
+  *(void **)(&ogg_stream_clear)=dlsym(opus_ogg_handle,"ogg_stream_clear");
+  *(void **)(&ogg_stream_reset)=dlsym(opus_ogg_handle,"ogg_stream_reset");
+  *(void **)(&ogg_stream_reset_serialno)=
+    dlsym(opus_ogg_handle,"ogg_stream_reset_serialn");
+  *(void **)(&ogg_stream_destroy)=dlsym(opus_ogg_handle,"ogg_stream_destroy");
+  *(void **)(&ogg_stream_check)=dlsym(opus_ogg_handle,"ogg_stream_check");
+  *(void **)(&ogg_stream_eos)=dlsym(opus_ogg_handle,"ogg_stream_eos");
+  *(void **)(&ogg_page_checksum_set)=
+    dlsym(opus_ogg_handle,"ogg_page_checksum_set");
+  *(void **)(&ogg_page_version)=dlsym(opus_ogg_handle,"ogg_page_version");
+  *(void **)(&ogg_page_continued)=dlsym(opus_ogg_handle,"ogg_page_continued");
+  *(void **)(&ogg_page_bos)=dlsym(opus_ogg_handle,"ogg_page_bos");
+  *(void **)(&ogg_page_eos)=dlsym(opus_ogg_handle,"ogg_page_eos");
+  *(void **)(&ogg_page_granulepos)=
+    dlsym(opus_ogg_handle,"ogg_page_granulepos");
+  *(void **)(&ogg_page_serialno)=dlsym(opus_ogg_handle,"ogg_page_serialno");
+  *(void **)(&ogg_page_pageno)=dlsym(opus_ogg_handle,"ogg_page_pageno");
+  *(void **)(&ogg_page_packets)=dlsym(opus_ogg_handle,"ogg_page_packets");
+  *(void **)(&ogg_packet_clear)=dlsym(opus_ogg_handle,"ogg_packet_clear");
 
   //
   // Initialize Encoder Instance
@@ -102,6 +191,41 @@ bool OpusCodec::startCodec()
     opus_encoder_ctl(opus_encoder,OPUS_SET_VBR(0));
     opus_encoder_ctl(opus_encoder,OPUS_SET_BITRATE(1000*bitrate()));
   }
+
+  ogg_stream_init(&opus_ogg_stream,rand());
+
+  //
+  // Header Packet
+  //
+  QByteArray info=MakeInfoHeader(channels(),streamSamplerate());
+  opus_ogg_packet.packet=(unsigned char *)info.constData();
+  opus_ogg_packet.bytes=info.length();
+  opus_ogg_packet.b_o_s=1;
+  opus_ogg_packet.e_o_s=0;
+  opus_ogg_packet.granulepos=0;
+  opus_ogg_packet.packetno=opus_packet_number++;
+  ogg_stream_packetin(&opus_ogg_stream,&opus_ogg_packet);
+  while(ogg_stream_flush(&opus_ogg_stream,&opus_ogg_page)) {
+    opus_header_pages.append((const char *)opus_ogg_page.header,opus_ogg_page.header_len);
+    opus_header_pages.append((const char *)opus_ogg_page.body,opus_ogg_page.body_len);
+  }
+
+  //
+  // Comment Packet
+  //
+  QByteArray comment=MakeCommentHeader();
+  opus_ogg_packet.packet=(unsigned char *)comment.constData();
+  opus_ogg_packet.bytes=comment.length();
+  opus_ogg_packet.b_o_s=0;
+  opus_ogg_packet.e_o_s=0;
+  opus_ogg_packet.granulepos=0;
+  opus_ogg_packet.packetno=opus_packet_number++;
+  ogg_stream_packetin(&opus_ogg_stream,&opus_ogg_packet);
+  while(ogg_stream_flush(&opus_ogg_stream,&opus_ogg_page)!=0) {
+    opus_header_pages.append((const char *)opus_ogg_page.header,opus_ogg_page.header_len);
+    opus_header_pages.append((const char *)opus_ogg_page.body,opus_ogg_page.body_len);
+  }
+
   return true;
 #else
   Log(LOG_ERR,"unsupported audio format (no build support)");
@@ -114,13 +238,81 @@ void OpusCodec::encodeData(Connector *conn,const float *pcm,int frames)
 {
 #ifdef HAVE_OPUS
   int s;
-  unsigned char data[8192];
+  unsigned char data[4096];
 
-  if((s=opus_encode_float(opus_encoder,pcm,frames,data,8192))>1) {
-    conn->writeData(frames,data,s);
+  if(opus_header_pages.size()>0) {
+    conn->writeData(0,(const unsigned char *)opus_header_pages.constData(),
+		    opus_header_pages.size());
+    opus_header_pages.clear();
+  }
+
+  if((s=opus_encode_float(opus_encoder,pcm,frames,data,4096))>1) {
+    opus_packet_granulepos+=frames;
+    opus_ogg_packet.packet=data;
+    opus_ogg_packet.bytes=s;
+    opus_ogg_packet.b_o_s=0;
+    opus_ogg_packet.e_o_s=0;
+    opus_ogg_packet.granulepos=opus_packet_granulepos;
+    opus_ogg_packet.packetno=opus_packet_number++;
+    ogg_stream_packetin(&opus_ogg_stream,&opus_ogg_packet);
+    while(ogg_stream_pageout(&opus_ogg_stream,&opus_ogg_page)!=0) {
+      conn->
+	writeData(frames,opus_ogg_page.header,opus_ogg_page.header_len);
+      conn->writeData(0,opus_ogg_page.body,opus_ogg_page.body_len);
+    }
   }
   else {
     Log(LOG_WARNING,QString().sprintf("opus encoding error %d",s));
   }
+
 #endif  // HAVE_OPUS
+}
+
+
+QByteArray OpusCodec::MakeInfoHeader(unsigned chans,unsigned samprate)
+{
+  QByteArray hdr(19,0);
+
+  hdr[0]='O';
+  hdr[1]='p';
+  hdr[2]='u';
+  hdr[3]='s';
+  hdr[4]='H';
+  hdr[5]='e';
+  hdr[6]='a';
+  hdr[7]='d';
+  hdr[8]=1;
+  hdr[9]=0xFF&chans;
+  hdr[12]=0xFF&samprate;
+  hdr[13]=0xFF&(samprate>>8);
+  hdr[14]=0xFF&(samprate>>16);
+  hdr[15]=0xFF&(samprate>>24);
+
+  return hdr;
+}
+
+
+QByteArray OpusCodec::MakeCommentHeader()
+{
+  QString version=QString("GlassCoder | ")+VERSION;
+  QByteArray hdr(12,0);
+  hdr[0]='O';
+  hdr[1]='p';
+  hdr[2]='u';
+  hdr[3]='s';
+  hdr[4]='T';
+  hdr[5]='a';
+  hdr[6]='g';
+  hdr[7]='s';
+  hdr[8]=version.toUtf8().length();
+  hdr[9]=0;
+  hdr[10]=0;
+  hdr[11]=0;
+  hdr.append(version.toUtf8());
+  hdr.append((char)0);
+  hdr.append((char)0);
+  hdr.append((char)0);
+  hdr.append((char)0);
+
+  return hdr;
 }
--- src/glasscoder/opuscodec.h
+++ src/glasscoder/opuscodec.h
@@ -22,6 +22,7 @@
 #define OPUSCODEC_H
 
 #ifdef HAVE_OPUS
+#include <ogg/ogg.h>
 #include <opus/opus.h>
 #endif  // HAVE_OPUS
 
@@ -43,8 +44,11 @@ class OpusCodec : public Codec
   void encodeData(Connector *conn,const float *pcm,int frames);
 
  private:
+  QByteArray MakeInfoHeader(unsigned chans,unsigned samprate);
+  QByteArray MakeCommentHeader();
 #ifdef HAVE_OPUS
   void *opus_handle;
+  void *opus_ogg_handle;
   OpusEncoder *opus_encoder;
   int (*opus_encoder_get_size)(int);
   OpusEncoder *(*opus_encoder_create)(opus_int32, int, int, int *);
@@ -56,6 +60,84 @@ class OpusCodec : public Codec
   void (*opus_encoder_destroy)(OpusEncoder *);
   int (*opus_encoder_ctl)(OpusEncoder *, int request,...);
   const char *(*opus_strerror)(int error);
+
+  void (*oggpack_writeinit)(oggpack_buffer *);
+  int (*oggpack_writecheck)(oggpack_buffer *);
+  void (*oggpack_writetrunc)(oggpack_buffer *,long);
+  void (*oggpack_writealign)(oggpack_buffer *);
+  void (*oggpack_writecopy)(oggpack_buffer *,void *,long);
+  void (*oggpack_reset)(oggpack_buffer *);
+  void (*oggpack_writeclear)(oggpack_buffer *);
+  void (*oggpack_readinit)(oggpack_buffer *,unsigned char *,int);
+  void (*oggpack_write)(oggpack_buffer *,unsigned long,int);
+  long (*oggpack_look)(oggpack_buffer *,int);
+  long (*oggpack_look1)(oggpack_buffer *);
+  void (*oggpack_adv)(oggpack_buffer *,int);
+  void (*oggpack_adv1)(oggpack_buffer *);
+  long (*oggpack_read)(oggpack_buffer *,int);
+  long (*oggpack_read1)(oggpack_buffer *);
+  long (*oggpack_bytes)(oggpack_buffer *);
+  long (*oggpack_bits)(oggpack_buffer *);
+  unsigned char *(*oggpack_get_buffer)(oggpack_buffer *);
+  void (*oggpackB_writeinit)(oggpack_buffer *);
+  int (*oggpackB_writecheck)(oggpack_buffer *);
+  void (*oggpackB_writetrunc)(oggpack_buffer *,long);
+  void (*oggpackB_writealign)(oggpack_buffer *);
+  void (*oggpackB_writecopy)(oggpack_buffer *,void *,long);
+  void (*oggpackB_reset)(oggpack_buffer *);
+  void (*oggpackB_writeclear)(oggpack_buffer *);
+  void (*oggpackB_readinit)(oggpack_buffer *,unsigned char *,int);
+  void (*oggpackB_write)(oggpack_buffer *,unsigned long,int);
+  long (*oggpackB_look)(oggpack_buffer *,int);
+  long (*oggpackB_look1)(oggpack_buffer *);
+  void (*oggpackB_adv)(oggpack_buffer *,int);
+  void (*oggpackB_adv1)(oggpack_buffer *);
+  long (*oggpackB_read)(oggpack_buffer *,int);
+  long (*oggpackB_read1)(oggpack_buffer *);
+  long (*oggpackB_bytes)(oggpack_buffer *);
+  long (*oggpackB_bits)(oggpack_buffer *);
+  unsigned char *(*oggpackB_get_buffer)(oggpack_buffer *);
+  int (*ogg_stream_packetin)(ogg_stream_state *, ogg_packet *);
+  int (*ogg_stream_iovecin)(ogg_stream_state *, ogg_iovec_t *,
+			    int,long, ogg_int64_t);
+  int (*ogg_stream_pageout)(ogg_stream_state *, ogg_page *);
+  int (*ogg_stream_flush)(ogg_stream_state *, ogg_page *);
+  int (*ogg_sync_init)(ogg_sync_state *);
+  int (*ogg_sync_clear)(ogg_sync_state *);
+  int (*ogg_sync_reset)(ogg_sync_state *);
+  int (*ogg_sync_destroy)(ogg_sync_state *);
+  int (*ogg_sync_check)(ogg_sync_state *);
+  char (*ogg_sync_buffer)(ogg_sync_state *, long);
+  int (*ogg_sync_wrote)(ogg_sync_state *, long);
+  long (*ogg_sync_pageseek)(ogg_sync_state *,ogg_page *);
+  int (*ogg_sync_pageout)(ogg_sync_state *, ogg_page *);
+  int (*ogg_stream_pagein)(ogg_stream_state *, ogg_page *);
+  int (*ogg_stream_packetout)(ogg_stream_state *,ogg_packet *);
+  int (*ogg_stream_packetpeek)(ogg_stream_state *,ogg_packet *);
+  int (*ogg_stream_init)(ogg_stream_state *,int);
+  int (*ogg_stream_clear)(ogg_stream_state *);
+  int (*ogg_stream_reset)(ogg_stream_state *);
+  int (*ogg_stream_reset_serialno)(ogg_stream_state *,int);
+  int (*ogg_stream_destroy)(ogg_stream_state *);
+  int (*ogg_stream_check)(ogg_stream_state *);
+  int (*ogg_stream_eos)(ogg_stream_state *);
+  void (*ogg_page_checksum_set)(ogg_page *);
+  int (*ogg_page_version)(const ogg_page *);
+  int (*ogg_page_continued)(const ogg_page *);
+  int (*ogg_page_bos)(const ogg_page *);
+  int (*ogg_page_eos)(const ogg_page *);
+  ogg_int64_t (*ogg_page_granulepos)(const ogg_page *);
+  int (*ogg_page_serialno)(const ogg_page *);
+  long (*ogg_page_pageno)(const ogg_page *);
+  int (*ogg_page_packets)(const ogg_page *);
+  void (*ogg_packet_clear)(ogg_packet *);
+
+  ogg_stream_state opus_ogg_stream;
+  ogg_page opus_ogg_page;
+  ogg_packet opus_ogg_packet;
+  uint64_t opus_packet_number;
+  uint64_t opus_packet_granulepos;
+  QByteArray opus_header_pages;
 #endif  // HAVE_OPUS
 };
 
