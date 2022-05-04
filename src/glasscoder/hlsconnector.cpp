b1e125dfcb5a1b80867ed3dc7f83b49f65d30ce3
Fred Gleason <fredg@paravelsystems.com>
2022-05-04 10:45:33 -0400

    2022-05-04 Fred Gleason <fredg@paravelsystems.com> 	* Fixed a regression in glasscoder(1) that caused the value of the 	'#EXTINF' tags to always be '0.00000' in HLS streams.
    Signed-off-by: Fred Gleason <fredg@paravelsystems.com>

--- src/glasscoder/hlsconnector.cpp
+++ src/glasscoder/hlsconnector.cpp
@@ -222,9 +222,9 @@ void HlsConnector::RotateMediaFile()
   if(!hls_config->serverNoDeletes()) {
     hls_media_datetimes[hls_sequence_back]=
       QDateTime(QDate::currentDate(),QTime::currentTime());
-    hls_media_durations[hls_sequence_back]=
-      (double)hls_media_frames/(double)audioSamplerate();
   }
+  hls_media_durations[hls_sequence_back]=
+    (double)hls_media_frames/(double)audioSamplerate();
   if((hls_sequence_back-hls_sequence_head)>=HLS_MINIMUM_SEGMENT_QUAN) {
     // Schedule garbage collection
     hls_media_killtimes[hls_sequence_head++]=hls_total_media_frames;
